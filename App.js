6db64c0a596dd251df53f74153e6be9c99f99436
eikyuu <v.duguet@laposte.net>
2020-11-01 23:31:38 +0100

    add redux-persist

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import Navigation from './components/navigation/Navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import Store from './store/configureStore';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
 
 export default function App() {
+  let persistor = persistStore(Store);
   return (
     <Provider store={Store}>
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,5 +1,14 @@
-import {combineReducers, createStore} from 'redux';
+import {createStore} from 'redux';
 import toggleFavorite from './reducers/favoriteReducer';
 import setAvatar from './reducers/avatarReducer';
+import {persistCombineReducers} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export default createStore(combineReducers({toggleFavorite, setAvatar}));
+const rootPersistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+export default createStore(
+  persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}),
+);
